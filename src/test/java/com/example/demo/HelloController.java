01f58cfda290b25176453938173c642c55ab5afd
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-11-28 18:40:03 +0530

    Update HelloController.java

--- src/test/java/com/example/demo/HelloController.java
+++ src/test/java/com/example/demo/HelloController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	
-	@RequestMapping("/hello")
-	public String message() {
-		return "Hello World";
-	}
+    @RequestMapping("/hello")
+    public String message() {
+        return "Hello World";
+    }
 }
